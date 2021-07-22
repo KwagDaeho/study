06bf4d41cf3fa2b1c42dcae211910a6a9c69c94c
user <user@userui-MacBookPro.local>
2021-07-22 21:15:19 +0900

    add:RandomNum.js

---
+++ js/bubble_sort.js
@@ -0,0 +1,3 @@
+
+var dataPool = Math.random();
+var dataList = [];
---
+++ js/random_num.js
@@ -0,0 +1,6 @@
+function RandomNum(minNum,maxNum){
+  var getRandomNum = (Math.floor((Math.random()*maxNum+1-minNum)+minNum));
+  return getRandomNum;
+};
+
+export default RandomNum;
