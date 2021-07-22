6c193ece7c38b989b65a49db8c9d3e9e6981defa
user <user@userui-MacBookPro.local>
2021-07-22 21:28:08 +0900

    test:err test

---
+++ js/common.js
@@ -0,0 +1,4 @@
+import RandomNum from "./random_num.js";
+
+const num1 = new RandomNum(2,5);
+console.log(num1);
--- js/random_num.js
+++ js/random_num.js
@@ -2,5 +2,4 @@ function RandomNum(minNum,maxNum){
   var getRandomNum = (Math.floor((Math.random()*maxNum+1-minNum)+minNum));
   return getRandomNum;
 };
-
 export default RandomNum;
